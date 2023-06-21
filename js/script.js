a16a5fc3e3bf7babf32102c7d15a7577a021b93f
carlosquiroz <carloshumberto.quiroz@estudioenpenascal.com>
2023-06-21 14:00:45 +0200

    cambios_210623_1400

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@ document.getElementById("icon_menu").addEventListener("click", mostrar_menu);
 
 function mostrar_menu(){
 
-    document.querySelector(".menu").classList.toggle("mostrar_menu");
+    document.querySelector(".menu1").classList.toggle("mostrar_menu");
     
 }
 
 window.onscroll = function () {
-    var posicion = window.pageYOffset || document.documentElement.scrollTop;
-    var elemento1 = document.getElementById("icon_heart");
-    var elemento2 = document.getElementById("icon_fire");
+    let posicion = window.scrollY || document.documentElement.scrollTop;
+    let elemento1 = document.getElementById("icon_heart");
+    let elemento2 = document.getElementById("icon_fire");
     elemento1.style.bottom = posicion * 0.1 + "px";
     elemento2.style.top = posicion * 0.1 + "px";
 }
